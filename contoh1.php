3984a4a2a45ef683bbe64afdb2251ca35dc3a28e
mrizkyy91 <90608921+mrizkyy91@users.noreply.github.com>
2021-09-20 16:18:23 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+ public function index()   
+ {
+     echo "<h1>Perkenalan Diri<h1/>";
+     echo " Nama: Muhammad rizky, alamat: jl raya bekasi, umur: 19 tahun";
+ }
+}    
+
