3781ec0de3c163173fbd59b5033dc6902bb960a8
mrizkyy91 <90608921+mrizkyy91@users.noreply.github.com>
2021-10-03 15:53:58 +0700

    pertemuan 4

--- Web.php
+++ Web.php
@@ -12,6 +12,14 @@ class Web extends CI_Controller{
         $data['judul'] = "Halaman Depan";
         $this->load->view('v_header',$data);
         $this->load->view('v_index',$data);
-        $this->load->view('v_footer',$data);
+        $this->load->view('v_footer',$data);    
     }
+
+    public function about(){
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
+    
 }    
