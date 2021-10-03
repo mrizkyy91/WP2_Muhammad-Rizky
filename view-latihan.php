e8e6f809f4717ea7b5e11675708d9a190fe70856
mrizkyy91 <90608921+mrizkyy91@users.noreply.github.com>
2021-10-03 15:04:32 +0700

    pertemuan1-3

---
+++ view-latihan.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Hallo Kawan.. Yuk kita Belajar Web Programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+    
